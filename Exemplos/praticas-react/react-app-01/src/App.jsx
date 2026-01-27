cbb2fdfb454e2080bc8e038fec7f28673b0c26c7
Lucas Mendes <mendes.lucas.lfm@gmail.com>
2026-01-27 18:43:12 -0300

    feat: inclui o primeiro exemplo sobre react, desenvolvido na aula de 27/01

---
+++ Exemplos/praticas-react/react-app-01/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import './App.css'
+
+export default function App() {
+
+  // será necessário armazenar a altura, o peso e o resultado do IMC como estados
+  // logo, as duas primeiras variáveis abaixo devem ser convertidas em estados
+  const altura = 0; // essa é uma variável comum do JavaScript, o React não controla o valor dela como estado
+  const peso = 0; // essa é uma variável comum do JavaScript, o React não controla o valor dela como estado
+  
+  // o hook useState cria um estado controlado pelo React
+  // ele devolve um array com duas posições:
+  // 1ª posição: o valor atual do estado
+  // 2ª posição: uma função para atualizar o valor do estado
+  const [imc, setImc] = useState(0);
+
+  const calcularIMC = (evento) => {
+    evento.preventDefault();
+    setImc(20); // atualiza o valor do estado imc para 20
+  }
+
+  return (
+    <>
+      <h1>Calculadora de IMC</h1>
+      <p>Informe seus dados no formulário abaixo.</p>
+
+      <form onSubmit={calcularIMC}>
+        <label htmlFor="altura">Altura</label>
+        <input type="number" id="altura" placeholder='Sua altuta em metros' step={0.01} min={0} />
+
+        <label htmlFor="peso">Peso</label>
+        <input type="number" id="peso" placeholder='Seu peso em kg' step={0.01} min={0} />
+
+        <button>Calcular</button>
+
+        { /** renderização condicional: só exibe o resultado se o imc for maior que 0
+          * perceba que usamos chaves {} para inserir código JavaScript dentro do JSX
+          */
+        }
+        { imc > 0 && (
+          <div className='resultado'>
+            <h2>Seu IMC é de {imc}</h2>
+          </div>
+        ) }
+      </form>
+    </>
+  )
+}
