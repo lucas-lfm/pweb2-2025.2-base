5b1b55fca7865f234136d8b996677fcc7b444619
Lucas Mendes <mendes.lucas.lfm@gmail.com>
2026-02-02 08:10:02 -0300

    feat: atualiza exemplo de formulário de IMC em React

--- Exemplos/praticas-react/react-app-01/src/App.jsx
+++ Exemplos/praticas-react/react-app-01/src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react';
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 export default function App() {
-
   // será necessário armazenar a altura, o peso e o resultado do IMC como estados
   // logo, as duas primeiras variáveis abaixo devem ser convertidas em estados
-  const altura = 0; // essa é uma variável comum do JavaScript, o React não controla o valor dela como estado
-  const peso = 0; // essa é uma variável comum do JavaScript, o React não controla o valor dela como estado
-  
+  const [altura, setAltura] = useState(0); // essa é uma variável comum do JavaScript, o React não controla o valor dela como estado
+  const [peso, setPeso] = useState(0); // essa é uma variável comum do JavaScript, o React não controla o valor dela como estado
+
   // o hook useState cria um estado controlado pelo React
   // ele devolve um array com duas posições:
   // 1ª posição: o valor atual do estado
@@ -16,8 +15,8 @@ export default function App() {
 
   const calcularIMC = (evento) => {
     evento.preventDefault();
-    setImc(20); // atualiza o valor do estado imc para 20
-  }
+    setImc((peso / (altura * altura)).toFixed(2));
+  };
 
   return (
     <>
@@ -25,24 +24,39 @@ export default function App() {
       <p>Informe seus dados no formulário abaixo.</p>
 
       <form onSubmit={calcularIMC}>
-        <label htmlFor="altura">Altura</label>
-        <input type="number" id="altura" placeholder='Sua altuta em metros' step={0.01} min={0} />
+        <label htmlFor='altura'>Altura</label>
+        <input
+          type='number'
+          id='altura'
+          placeholder='Sua altura em metros'
+          step={0.01}
+          min={0}
+          value={altura}
+          onChange={(e) => setAltura(e.target.value)}
+        />
 
-        <label htmlFor="peso">Peso</label>
-        <input type="number" id="peso" placeholder='Seu peso em kg' step={0.01} min={0} />
+        <label htmlFor='peso'>Peso</label>
+        <input
+          type='number'
+          id='peso'
+          placeholder='Seu peso em kg'
+          step={0.01}
+          min={0}
+          value={peso}
+          onChange={(e) => setPeso(e.target.value)}
+        />
 
         <button>Calcular</button>
 
-        { /** renderização condicional: só exibe o resultado se o imc for maior que 0
-          * perceba que usamos chaves {} para inserir código JavaScript dentro do JSX
-          */
-        }
-        { imc > 0 && (
+        {/** renderização condicional: só exibe o resultado se o imc for maior que 0
+         * perceba que usamos chaves {} para inserir código JavaScript dentro do JSX
+         */}
+        {imc > 0 && (
           <div className='resultado'>
             <h2>Seu IMC é de {imc}</h2>
           </div>
-        ) }
+        )}
       </form>
     </>
-  )
+  );
 }
